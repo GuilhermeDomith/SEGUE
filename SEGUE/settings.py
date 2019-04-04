e5edae091456559c11c836c9bcb4eb7b33d73a3d
Guilherme Domith <guilhermedomith@gmail.com>
2019-04-03 23:47:23 -0300

    [ADD] Custom Admin APP

--- SEGUE/settings.py
+++ SEGUE/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'egresso',
     'curso',
     'oportunidade',
+    'custom_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
