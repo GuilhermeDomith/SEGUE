4dc8f414ab9e45a9ee53afc1841e06b5c265ab6d
guilherme <guilhermedomith@gmail.com>
2019-04-04 13:16:45 -0300

    [UP] Redirecionamento após login de acordo com tipo de usuário

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth import views as auth_views
 app_name = 'account'
 
 urlpatterns = [   
-    path('login/', auth_views.LoginView.as_view(), name='login'), 
+    path('login/', views.MyLoginView.as_view(), name='login'), 
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
